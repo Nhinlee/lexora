941bfaf698bdc2439b58d461ed1d426c95f72a80
Nhinlee <nhinlechi@gmail.com>
2025-11-21 23:17:36 +0700

    build: update compiled output files.

--- apps/api/dist/ai/ai.service.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class AiService {
-    private readonly logger;
-    private readonly client;
-    constructor();
-    extractVocabulary(imageBuffer: Buffer): Promise<any[]>;
-}
--- apps/api/dist/search/search.service.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class SearchService {
-    private readonly logger;
-    private readonly apiKey;
-    searchImage(query: string): Promise<string | null>;
-}
